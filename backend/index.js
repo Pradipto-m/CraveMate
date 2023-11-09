f3a6f011fd636d3da71740c1ed07dc61ed4f4af6
Pradipto Mondal <pro.edu08@gmail.com>
2023-11-09 22:02:48 +0530

    Create user signup post request

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose'); 
 const authRouter = require('./routes/authRouter');
 
+// Init
 const PORT = 3000;
 const app = express();
+const dbUrl = "";
 
+// Connections
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
 });
 
+mongoose.connect(dbUrl).then(() => {
+    console.log("Connection Successful");
+}).catch((e) => {
+    console.log(e);
+});
+
+// Middleware
 app.use(authRouter);
 
+// GET response
 app.get('/', (req, res) => {
     res.json({ 'msg' : 'Hello World!' });
 });
--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const authRouter = express.Router();
 
-authRouter.get('/user', (req, res) => {
-    res.json({ 'name' : 'Pradipto', 'age' : 22 });
+authRouter.post('/api/user/signup', (req, res) => {
+    const {username, email, password} = req.body;
 });
+
+// authRouter.get('/api/user', (req, res) => {
+//     res.json({ 'name' : 'Pradipto', 'age' : 22 });
+// });
 
 module.exports = authRouter;
